4b4e56cc96037980ec938eed1406b53ae687e7ba
Christian Fritz <christian.fritz@gmail.com>
2018-09-21 21:56:38 -0700

    C and C++ support
    - incl. C++11
    - added some missing (sample) files
    - some cleanup

--- spec/fixtures/sample.cpp
+++ spec/fixtures/sample.cpp
@@ -1,18 +1,63 @@
 
-class Test {
-
-public:
-  void test();
-
-private:
-  void more();
-
-};
 
 void main(int argc, char** argv) {
+  int i = 9;
+
+  int i =
+    6;
+
+  int ar[2] = {1,
+    2};
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d", i);
+    cout
+      << "at 1"
+      << "at 1"
+      << std::end;
+    cin
+      >> i;
+    cin >>
+      j;
+  }
+
+  for (
+    int i = 0;
+    i < 10;
+    i++)
+    cout << i;
 
 
-return 1;
+  for (int i = 0;
+    i < 10; i++) {
+      cout << i;
+    }
+
+  auto f1 = [](int x, int y) -> int {
+    return x + y;
+  }
+
+  auto f2 = [](int x, int y)
+    -> int {
+      return x + y;
+    }
+
+  return
+    1
+      + 3;
+
+  bool truth =
+    (a(tree) &&
+      b(tree)) ||
+      (c(&self) &&
+        d());
+}
+
+
+void main(int argc,
+  char** argv) {
+  int i = 9;
+  return 1;
 }
 
 
@@ -25,6 +70,11 @@ void main() {
     printf("ugh");
   }
 
+  if (no_compound_statement)
+    printf("woot");
+  else
+    printf("what else?");
+
   if (alternate_style_indentation)
   {
     printf("still works");
@@ -36,7 +86,7 @@ void main() {
 
   int test_function = function_with_param(this_param,
     that_param
-  ); // strange dedent after typing semicolon
+  );
 
   int test_function = function_with_param(
     this_param,
@@ -45,10 +95,7 @@ void main() {
 
   int test_function = function_with_proper_indent(param1,
     param2,
-  ); // strange dedent again
-  uh_oh = 1;
-
-  still_after_blank_line = 1;
+  );
 
   return;
 }
@@ -60,17 +107,192 @@ struct rec {
   char id[26];
   struct year *ptr; };
 
-struct year {
-  int year;
-  struct year *next;
-  struct day *ptr; };
-  
 struct day {
   int day;
-  double expenses;
-  struct movie *ptr;
-  struct day *next; };
+  struct day *next;
+};
 
-struct movie {
+struct movie
+{
   char id[5];
-  struct movie *next; };
+  struct movie *next;
+};
+
+
+class Test {
+
+public:
+  void test();
+
+private:
+  void more();
+};
+
+
+static void destroy_value(Rule *rule) {
+  switch (rule->type) {
+    case Rule::BlankType: return rule->blank_.~Blank();
+    case Rule::CharacterSetType: return rule->character_set_.~CharacterSet();
+  }
+}
+
+Rule &Rule::operator=(const Rule &other) {
+  destroy_value(this);
+  type = other.type;
+  switch (type) {
+    case BlankType:
+    new (&blank_) Blank(other.blank_);
+    break;
+    case CharacterSetType:
+    new (&character_set_) CharacterSet(other.character_set_);
+    break;
+  }
+  return *this;
+}
+
+
+new_rule.match(
+  [elements](Choice choice) {
+    for (auto &element : choice.elements) {
+      add_choice_element(elements, element);
+    }
+  },
+
+  [elements](auto rule) {
+    for (auto &element : *elements) {
+      if (element == rule) return;
+    }
+    elements->push_back(rule);
+  }
+);
+
+
+Rule Rule::choice(const vector<Rule> &rules) {
+  vector<Rule> elements;
+  for (auto &element : rules) {
+    add_choice_element(&elements, element);
+  }
+  return (elements.size() == 1) ? elements.front() : Choice{elements};
+}
+
+Rule Rule::repeat(const Rule &rule) {
+  return rule.is<Repeat>() ? rule : Repeat{rule};
+}
+
+Rule Rule::seq(const vector<Rule> &rules) {
+  Rule result;
+  for (const auto &rule : rules) {
+    rule.match(
+      [](Blank) {},
+      [&](Metadata metadata) {
+        if (!metadata.rule->is<Blank>()) {
+          result = Seq{result, rule};
+        }
+      },
+      [&](auto) {
+        if (result.is<Blank>()) {
+          result = rule;
+        } else {
+          result = Seq{result, rule};
+        }
+      }
+    );
+  }
+  return result;
+}
+
+namespace myspace {
+
+size_t hash<CharacterSet>::operator()(const CharacterSet &character_set) const {
+  size_t result = 0;
+  for (uint32_t c : character_set.included_chars) {
+    hash_combine(&result, c);
+  }
+}
+
+}
+
+
+enum TokenType {
+  RAW_STRING_LITERAL,
+  ANOTHER,
+};
+
+extern "C" {
+
+void *tree_sitter_cpp_external_scanner_create() {
+  return new Scanner();
+}
+
+[sym_raw_string_literal] = {
+  .visible = true,
+  .named = true,
+};
+
+static const TSSymbolMetadata ts_symbol_metadata[] = {
+  [sym_raw_string_literal] = {
+    .visible = true,
+    .named = true,
+  },
+  [ts_builtin_sym_end] = {
+    .visible = false,
+    .named = true,
+  },
+}
+
+
+} // extern "C"
+
+/* Comment
+  line
+*/
+
+void foo() {
+  char** lookahead;
+  if (4
+    + 5 < 10) {
+    pass;
+  }
+
+  if (4 + 5
+    < 10) {
+    pass;
+  }
+
+  doit
+    .right
+    .now();
+
+  if (('0' <= lookahead && lookahead <= '9') ||
+    ('a' <= lookahead && lookahead <= 'z'))
+    ADVANCE();
+
+}
+
+struct AltStruct
+{
+  AltStruct(int x, double y):
+    x_{x}
+    , y_{y}
+  {}
+
+private:
+  int x_;
+  double y_;
+};
+
+BasicStruct var1{
+  5,
+  3.2};
+AltStruct var2{2,
+  4.3};
+
+
+template <typename Second>
+typedef SomeType<OtherType,
+  Second,
+  5> TypedefName;
+
+// tuple types
+typedef std::tuple <int,
+  double, long &, const char *> test_tuple;
